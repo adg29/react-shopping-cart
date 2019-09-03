39a1f9d4aeb5e8812a8353ae04a48b6f957281c0
Alan Garcia <alandavidgarcia@gmail.com>
2019-09-02 23:57:05 -0400

    MultiSelect component wired with redux connect and mapStateToProps

---
+++ src/components/Shelf/MultiSelect/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+import { updateSelect } from '../../../services/multiselect/actions'
+
+import Select from 'react-select';
+import tagOptions from './data/sprayartnyc-tags.json'
+
+const MultiSelect = ({ updateSelect, select }) => {
+  const handleChange = (selectedOption) => {
+    console.log(selectedOption)
+  }
+  return (
+    <div>
+    {console.log(tagOptions)}
+    <Select
+      onChange={handleChange}
+      isMulti
+      name="tags"
+      options={tagOptions}
+      className="basic-multi-select"
+      classNamePrefix="select"
+    />
+    </div>
+  )
+}
+
+MultiSelect.propTypes = {
+  updateSelect: PropTypes.func.isRequired,
+  select: PropTypes.string.isRequired
+}
+
+const mapStateToProps = state => ({
+  select: state.sort.select
+})
+
+export default connect(
+  mapStateToProps,
+  { updateSelect }
+)(MultiSelect)
